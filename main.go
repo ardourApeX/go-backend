1479b95d9cf6828684c49227ac2bd72918d44973
Joy <joygupta501@gmail.com>
2024-01-15 04:57:35 +0530

    Feat : Added server

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {   
+    fmt.Fprintf(w, "World!")
+}
+
+
+func main() {
+    http.HandleFunc("/hello", helloHandler)
+    fmt.Printf("Starting server at port 8080\n")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
+}
