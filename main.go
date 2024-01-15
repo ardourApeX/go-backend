60583b277ecf9e7cc13dd277dec5593b7e72c177
Joy <joygupta501@gmail.com>
2024-01-15 22:41:04 +0530

    Bug : Fixing railway app

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+	// Specify the default port
+	defaultPort := 3000
+
+	// Parse the port flag
+	portPtr := flag.Int("port", defaultPort, "server port to listen on")
+	flag.Parse()
+
+	// Start the server and listen for incoming requests
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
 
-	// Specify the port to listen on
-	port := 3000
+	// Construct the server address
+	serverAddr := fmt.Sprintf(":%d", *portPtr)
 
-	// Start the server and listen for incoming requests
-	fmt.Printf("Server is running on :%d...\n", port)
-	err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), nil)
+	fmt.Printf("Server is running on %s...\n", serverAddr)
+
+	err := http.ListenAndServe(serverAddr, nil)
 	if err != nil {
 		fmt.Printf("Error starting server: %v\n", err)
 		return
 	}
-
 }
